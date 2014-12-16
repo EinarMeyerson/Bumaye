c4db78723fecf89127ea0830d0b0cd249c872ba7
majouni <majouni@majouni-AS5738ZG>
2014-12-16 16:04:22 +0100

    puta mierda

--- Bumaye-android/src/ea/grupo2/Bumaye/android/api/MediaType.java
+++
@@ -1,7 +0,0 @@
-package ea.grupo2.Bumaye.android.api;
-
-public class MediaType {
-	// Media type
-	public final static String API_USER = "application/vnd.bumaye.api.user+json";
-	public final static String API_PERSONAJE = "application/vnd.bumaye.api.personaje+json";
-}
